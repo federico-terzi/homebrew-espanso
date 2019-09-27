2544e035a3cc4d3570297e450ee663fc558d7ccf
vsts <Federico Terzi>
2019-09-27 22:01:11 +0000

    Update to version: 0.2.0

--- Formula/espanso.rb
+++ Formula/espanso.rb
@@ -5,8 +5,8 @@ class Espanso < Formula
   desc "Cross-platform Text Expander written in Rust"
   homepage "https://github.com/federico-terzi/espanso"
   url "https://github.com/federico-terzi/espanso/releases/latest/download/espanso-mac.tar.gz"
-  sha256 "8a7499e31e32a8c3e1c7151739e224f6cb522a50d51f7b988d3ce780ba94ada2"
-  version "0.1.2"
+  sha256 "7a63c560f11664b666690f810f12b32a7f5f359dc0e3c72244bbb886071a42f8"
+  version "0.2.0"
 
   def install
     bin.install "espanso"
