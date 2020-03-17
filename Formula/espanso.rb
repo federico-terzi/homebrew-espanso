688411ec8aefb8eb19c121075f16fdb660816f5f
runner <Federico Terzi>
2020-03-17 20:46:27 +0000

    Update to version: 0.5.3

--- Formula/espanso.rb
+++ Formula/espanso.rb
@@ -5,7 +5,7 @@ class Espanso < Formula
   desc "Cross-platform Text Expander written in Rust"
   homepage "https://github.com/federico-terzi/espanso"
   url "https://github.com/federico-terzi/espanso/releases/latest/download/espanso-mac.tar.gz"
-  sha256 "16cbb9505a6134e603cca37571b3d5662016772433b79caad3b6684c48bee065"
+  sha256 "cf2215ff23cf0b86d73933abb55ad4d188551dcb20979c4e6f27ef2e2885c5e7"
   version "0.5.3"
   depends_on "openssl@1.1"
 
